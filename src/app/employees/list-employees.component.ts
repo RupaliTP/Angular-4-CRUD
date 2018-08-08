d02676cc07e78924a75c0230d242ad584d33ac65
RupaliTP <42169953+RupaliTP@users.noreply.github.com>
2018-08-08 19:19:54 +0530

    Update list-employees.component.ts

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -11,56 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListEmployeesComponent implements OnInit {
   private selectedEmployeeId: number;
-  employees: Employee[] = [
-    //   {
-    //     id:'1',
-    //     name:'Rupali Tidke',
-    //     gender:'Female',
-    //     email:'rupali@gmail.com',
-    //     phoneNumber:2233111,
-    //     contactPreference:'Email',
-    //     dateOfBirth:new Date('10/10/1992'),
-    //     department:'IT',
-    //     isActive:true,
-    //     photoPath:'assets/images/blank_profile_female.jpg',
-    // },
-    //   {
-    //     id:'2',
-    //     name:'Surbhi Takzare',
-    //     gender:'Female',
-    //     email:'surbhi@gmail.com',
-    //     phoneNumber:3653636,
-    //     contactPreference:'PhoneNumber',
-    //     dateOfBirth:new Date('09/11/1992'),
-    //     department:'IT',
-    //     isActive:true,
-    //     photoPath:'assets/images/blank_profile_female.jpg',
-    // },
-    //   {
-    //     id:'3',
-    //     name:'Rohan Kulkarni',
-    //     gender:'Male',
-    //     email:'rohan@gmail.com',
-    //     phoneNumber:3388833,
-    //     contactPreference:'Email',
-    //     dateOfBirth:new Date('05/02/1992'),
-    //     department:'IT',
-    //     isActive:true,
-    //     photoPath:'assets/images/default-profile-pic.png',
-    //   },
-    //   {
-    //     id:'4',
-    //     name:'Rohit Chauhan',
-    //     gender:'Male',
-    //     email:'rohit@gmail.com',
-    //     phoneNumber:887677,
-    //     contactPreference:'PhoneNumber',
-    //     dateOfBirth:new Date('01/02/1992'),
-    //     department:'IT',
-    //     isActive:true,
-    //     photoPath:'assets/images/default-profile-pic.png',
-    //   },
-  ];
+  employees: Employee[] = [];
 
   constructor(private _employeeService: EmployeeService,
     private _route: Router,
